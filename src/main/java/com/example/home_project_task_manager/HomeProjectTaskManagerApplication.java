8d014806508d0202d206ffd5d415a6c19f7bdd23
Vlad <V.G.Vranceanu@student.tudelft.nl>
2023-12-04 13:29:12 +0100

    Initial commit

---
+++ src/main/java/com/example/home_project_task_manager/HomeProjectTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.home_project_task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeProjectTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeProjectTaskManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/home_project_task_manager/HomeProjectTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home_project_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeProjectTaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
